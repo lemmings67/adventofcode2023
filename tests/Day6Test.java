054482818edcba3bd8d4e75956920ccbf341568b
Pascal EISELE <peisele@appligos.fr>
2023-12-06 21:42:56 +0100

    Tests for Day6

---
+++ tests/Day6Test.java
@@ -0,0 +1,18 @@
+// Junit test for Day1 class 
+
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.nwolf.adventofcode2023.Day6;
+import java.io.File;
+
+public class Day6Test {
+
+    @Test
+    public void test() {
+        Day6 d = new Day6(new File("data/day6_testdata.txt"));
+        assertEquals(288, d.part1());
+        assertEquals(71503, d.part2());
+    }
+}
