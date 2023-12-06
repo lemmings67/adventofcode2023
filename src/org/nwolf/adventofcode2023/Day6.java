70091c9bae6d69cbdaac2f234cf3243f39544a86
Pascal EISELE <peisele@appligos.fr>
2023-12-06 21:42:35 +0100

    Part 2 : completed

--- src/org/nwolf/adventofcode2023/Day6.java
+++ src/org/nwolf/adventofcode2023/Day6.java
@@ -10,12 +10,12 @@ public class Day6 {
     int times[];
     int distances[];
     int race_winning_combination[];
-            
+
     // Hold boat button 6 for 1 ms
     final int max_distance_for_holding_1s = 6;
 
     public Day6(File file) {
-         try {
+        try {
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
@@ -44,14 +44,15 @@ public class Day6 {
         }
     }
 
-    public boolean isWinningCombination(int holding_time, int distance, int max_time) {
-        int speed = holding_time;
-        int remaining_time = max_time - holding_time;
+    public boolean isWinningCombination(long holding_time, long distance, long max_time) {
+        long speed = holding_time;
+        long remaining_time = max_time - holding_time;
 
-        int distance_reached = speed*remaining_time;
+        long distance_reached = speed * remaining_time;
 
-        if (distance_reached > distance) return true;
-        
+        if (distance_reached > distance)
+            return true;
+
         return false;
     }
 
@@ -61,18 +62,37 @@ public class Day6 {
             int distance = this.distances[i];
             int max_time = this.times[i];
             for (int holding_time = 1; holding_time < max_time; holding_time++) {
-                if (isWinningCombination(holding_time, distance, max_time)) race_winning_combination[i]++;
+                if (isWinningCombination(holding_time, distance, max_time))
+                    race_winning_combination[i] += 1;
             }
-                    System.out.println("Race " + i + " : " + race_winning_combination[i]);
+            System.out.println("Race " + i + " : " + race_winning_combination[i]);
 
         }
 
         int winning_combination = race_winning_combination[0];
         for (int i = 1; i < this.distances.length; i++) {
-            winning_combination = winning_combination*race_winning_combination[i];
+            winning_combination = winning_combination * race_winning_combination[i];
         }
 
         return winning_combination;
+    }
+
+    public long part2() {
+        String distance_str="";
+        String max_time_str="";
+        for (int i = 0; i < this.distances.length; i++) {
+            distance_str += this.distances[i];
+            max_time_str += this.times[i];
+        }
+        long distance = Long.parseLong(distance_str);
+        long max_time = Long.parseLong(max_time_str);
+    
+        long race_winning_combination = 0;
+        for (int holding_time = 1; holding_time < max_time; holding_time++) {
+                if (isWinningCombination(holding_time, distance, max_time)) race_winning_combination += 1;
+        }
+
+        return race_winning_combination;
     }
 
     public static void main(String[] args) {
@@ -80,6 +100,7 @@ public class Day6 {
         Date start_date = new Date();
         Day6 day6 = new Day6(new File("data/day6_input.txt"));
         System.out.println("Part 1: " + day6.part1());
+        System.out.println("Part 2: " + day6.part2());
         Date end_date = new Date();
         System.out.println("Execution time: " + (end_date.getTime() - start_date.getTime()) + " ms");
     }
